9972ac0f4476fd957f50af38591adc1f223b2180
Oussamabssd <oussamakanazawa@gmail.com>
2025-02-15 21:51:29 +0000

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,97 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+import moment from "moment";
+import Loading from "./Loading";
+import "./i18n";
+import { useTranslation } from "react-i18next";
+moment.locale("ar");
+
+let cancelAxios = null;
 
 function App() {
+  const {t, i18n} = useTranslation();
+  const [loading, setLoading] = useState(true);
+  const currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
+  console.log(currentDate);
+  const [weather, setWeather] = useState({
+    description: "",
+    temp: null,
+    temp_min: null,
+    temp_max: null,
+    weatherIcon: null,
+  });
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?lat=36.7525&lon=3.04197&appid=589f1c1e140219b2ab9d96e5b98cfd61",
+        {
+          cancelToken: new axios.CancelToken((c) => (cancelAxios = c)),
+        }
+      )
+      .then((response) => {
+        const icon = response.data.weather[0].icon;
+        setLoading(false);
+        setWeather({
+          description: response.data.weather[0].description,
+          temp: Math.round(response.data.main.temp - 273.15),
+          temp_min: Math.round(response.data.main.temp_min - 273.15),
+          temp_max: Math.round(response.data.main.temp_max - 273.15),
+          weatherIcon: `https://openweathermap.org/img/wn/${icon}.png`,
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
+  }, []);
   return (
     <>
       {/* page */}
-      <div className="h-screen w-screen bg-blue-700">
-        {/* container */}
-        <div className="container h-screen w-md m-auto flex flex-col justify-center items-start">
-          {/* card */}
-          <div className="h-70 w-full bg-blue-900 rounded-lg flex flex-col p-2 shadow-xl">
-            {/* header card */}
-            <div className="flex flex-row-reverse items-end px-6">
-              <p className="text-6xl font-500 my-2">الرياض</p>
-              <h2 className="mr-2">مايو 29 2024</h2>
-            </div>
-            {/*== header card ==*/}
-            <hr className="my-3" />
-            {/* body card */}
-            <div className="h-full w-full flex flex-row px-6">
-              <div className="h-full w-1/2"></div>
-              <div className="h-full w-1/2 flex flex-col">
-                <div className="flex flex-row-reverse justify-start items-center h-1/2 w-full">
-                  <p className="text-8xl">38</p>
-                  <div>{/* TODO: image */}</div>
+      {loading && <Loading />}
+      {!loading && (
+        <div className="h-screen w-screen bg-blue-700">
+          {/* container */}
+          <div className="container h-screen w-md m-auto flex flex-col justify-center items-start">
+            {/* card */}
+            <div className="h-70 w-full bg-blue-900 rounded-lg flex flex-col p-2 shadow-xl">
+              {/* header card */}
+              <div className="flex flex-row-reverse items-end px-6">
+                <p className="text-6xl font-500 my-2">جيجل</p>
+                <h2 className="mr-2">{currentDate}</h2>
+              </div>
+              {/*== header card ==*/}
+              <hr className="my-3" />
+              {/* body card */}
+              <div className="h-full w-full flex flex-row px-6">
+                <div className="h-full w-1/2 ">
+                  <img className="h-full " src={weather.weatherIcon} />
                 </div>
-                <div className="flex flex-col justify-evenly items-end h-1/2 w-full">
-                  <p className="text-xl">broken cloud</p>
-                  <p className="text-sm">الصغرى: 38 | الكبرى: 38</p>
+                <div className="h-full w-1/2 flex flex-col">
+                  <div className="flex flex-row-reverse justify-start items-center h-1/2 w-full">
+                    <p className="text-8xl">{weather.temp}</p>
+                    <div className="h-full w-full">
+                      <img src={weather.weatherIcon} alt="" />
+                    </div>
+                  </div>
+                  <div className="flex flex-col justify-evenly items-end h-1/2 w-full">
+                    <p className="text-xl">{weather.description}</p>
+                    <p className="text-sm">
+                      الصغرى: {weather.temp_min} | الكبرى: {weather.temp_max}
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div>{/* TODO: image */}</div>
+              {/*== body card ==*/}
             </div>
-            {/*== body card ==*/}
+            {/*== card ==*/}
+            <button className="py-2 text-white rounded-lg cursor-pointer">
+              انجليزي
+            </button>
           </div>
-          {/*== card ==*/}
-          <button className="py-2 text-white rounded-lg cursor-pointer">
-            انجليزي
-          </button>
+          {/*== container ==*/}
         </div>
-        {/*== container ==*/}
-      </div>
+      )}
       {/* == page == */}
     </>
   );
---
+++ src/Loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="h-screen w-screen bg-blue-700 flex justify-center items-center">
+      <h2>Loading...</h2>
+    </div>
+  );
+}
